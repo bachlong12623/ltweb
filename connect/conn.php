7a45da0d3db7c060ece27392bc2e14a4bd2f6250
Cyonz <95512602+Cyonz@users.noreply.github.com>
2021-12-04 10:21:00 +0700

    Create conn.php

---
+++ connect/conn.php
@@ -0,0 +1,13 @@
+<?php
+    $severname="localhost";
+    $username = "root";
+    $password = "";
+    $database = "bc_10/12";
+     
+    $conn = mysqli_connect($severname,$username,$password,$database);
+    if(!$conn){
+        echo ("Kết nối không thành công");
+    }else{
+        echo ("kết nối thành công! ");
+    }
+>?
