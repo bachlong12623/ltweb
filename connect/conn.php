e2967c2cc8ebee2b2d756238c04cd13675debc3c
bachlong12623 <bachlong12623@gmail.com>
2021-12-04 10:51:59 +0700

    Update conn.php

--- connect/conn.php
+++ connect/conn.php
@@ -5,9 +5,4 @@
     $database = "bc_10/12";
      
     $conn = mysqli_connect($severname,$username,$password,$database);
-    if(!$conn){
-        echo ("Kết nối không thành công");
-    }else{
-        echo ("kết nối thành công! ");
-    }
 ?>
