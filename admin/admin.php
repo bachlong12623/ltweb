3d23d8c4baacec13305f426a53bba5ecbc9f59b5
bachlong12623 <bachlong12623@gmail.com>
2021-12-04 11:07:59 +0700

    a

--- admin/admin.php
+++ admin/admin.php
@@ -6,32 +6,38 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Admin</title>
+	<link rel="stylesheet" href="../css/admin.css">
 </head>
 
 <body>
-	<table>
-		<tr>
-			<th>MST</th>
-			<th>Hinh anh</th>
-			<th>Tieu de</th>
-			<th>Noi dung</th>
-		</tr>
-		<tr>
-			<?php
-			include "../connect/conn.php";
-			$sql = "SELECT * FROM `tin_tuc`";
-			$query = mysqli_query($conn,$sql);
-			print_r($query);
-			while($row = mysqli_fetch_array($query)){
+	<div class="container">
 
-			?>
-			<td><?php echo $row['MST'] ?></td>
-			<td><?php echo $row['Hinh_Anh'] ?></td>
-			<td><?php echo $row['Tieu_De'] ?></td>
-			<td><?php echo $row['Noi_Dung'] ?></td>
-			<?php } ?>
+		<table>
+			<tr>
+				<th>MST</th>
+				<th>Hinh anh</th>
+				<th>Tieu de</th>
+				<th>Noi dung</th>
+			</tr>
+			<tr>
+				<?php
+				include "../connect/conn.php";
+				$sql = "SELECT * FROM `tin_tuc`";
+				$query = mysqli_query($conn, $sql);
+				while ($row = mysqli_fetch_array($query)) {
+
+				?>
+			<tr>
+				<td><?php echo $row['MST'] ?></td>
+				<td><a href=""></a></td>
+				<td><?php echo $row['Hinh_Anh'] ?></td>
+				<td><?php echo $row['Tieu_De'] ?></td>
+				<td><?php echo $row['Noi_Dung'] ?></td>
+			</tr>
+		<?php } ?>
 		</tr>
-	</table>
+		</table>
+	</div>
 </body>
 
 </html>
