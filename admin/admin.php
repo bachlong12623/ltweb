390799723005d9e8492301cb72cc9bbc965e4487
bachlong12623 <bachlong12623@gmail.com>
2021-12-04 10:48:11 +0700

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -1,14 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Admin</title>
 </head>
+
 <body>
 	<table>
-		
+		<tr>
+			<th>MST</th>
+			<th>Hinh anh</th>
+			<th>Tieu de</th>
+			<th>Noi dung</th>
+		</tr>
+		<tr>
+			<?php
+			include "../connect/conn.php";
+			$sql = "SELECT * FROM `tin_tuc`";
+			$query = mysqli_query($conn,$sql);
+			while($row = mysqli_fetch_array($query)){
+
+			?>
+			<td><?php $row['MST'] ?></td>
+			<td><?php $row['Hinh_Anh'] ?></td>
+			<td><?php $row['Tieu_De'] ?></td>
+			<td><?php $row['Noi_Dung'] ?></td>
+			<?php } ?>
+		</tr>
 	</table>
 </body>
+
 </html>
