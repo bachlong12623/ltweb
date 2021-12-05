6280e81dce5dc35b6e1ffb53a76aefe442c1d51a
bachlong12623 <bachlong12623@gmail.com>
2021-12-05 09:49:01 +0700

    a

---
+++ block/header.php
@@ -0,0 +1,19 @@
+
+    <nav>
+        <div class="container">
+            <div class="row">
+                <div class="menu-box">
+                    <h1 class="logo"><a href="../nhom2/index.php"><img src="img/pqe2.png" alt="" /></a></h1>
+                    <div class="nav-menu">
+                        <ul id="menu">
+                            <li><a href="index.php">Trang chủ</a></li>
+                            <li><a href="index.php?url=datve">Đặt vé Online</a></li>
+                            <li><a href="index.php?url=huongdan">Hướng dẫn đặt vé</a></li>
+                            <li><a href="index.php?url=dangki">Đăng ký đại lý</a></li>
+                            <li><a href="index.php?url=lienhe">Liên hệ</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
