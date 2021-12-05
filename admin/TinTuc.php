4319d7459e5388162ce3459856e7dded1adca707
bachlong12623 <bachlong12623@gmail.com>
2021-12-05 11:27:50 +0700

    a

--- admin/TinTuc.php
+++ admin/TinTuc.php
@@ -18,6 +18,7 @@
 		$loaitin = $_POST['LoaiTin'];
 		echo $loaitin;
 
+$sql = "INSERT INTO `tin_tuc` (`MST`, `Loai_Tin`, `Hinh_Anh`, `Tieu_De`, `Noi_Dung`)
 VALUES (NULL, '$loaitin', '$hinhanh', '$tieude', '$noidung')";
 		if ($conn->query($sql)) {
 			echo "<script>alert('Thêm Thành Công!');</script>";
