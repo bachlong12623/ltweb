6862c2d49f3d3d80e43069a419df4ae5fd2accd1
Cyonz <95512602+Cyonz@users.noreply.github.com>
2021-12-08 09:46:09 +0700

    Create Chi_Tiet_Tin.php

---
+++ Body/Chi_Tiet_Tin.php
@@ -0,0 +1,29 @@
+<?php
+require '../connect/conn.php';
+require '../block/index_func.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/css_tin.css">
+</head>
+
+<body>
+    
+    <?php
+    include "../block/header.php";
+    ?>
+     <div class= container>
+         
+     </div>
+     
+
+</body>
+
+</html>
