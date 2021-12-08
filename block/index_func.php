ecfb9fb567acd25db33fe5fec5446b2d152ac254
Cyonz <95512602+Cyonz@users.noreply.github.com>
2021-12-08 08:38:58 +0700

    Create index_func.php

---
+++ block/index_func.php
@@ -0,0 +1,9 @@
+<?php
+ function laytin($n,$conn)
+ {
+       $sql="select * from tin_tuc order by MST desc limit $n";
+       $result=mysqli_query($conn,$sql);
+       return $result;
+ }
+ function tintheoloai($Loai_Tin,$conn)
+?>
