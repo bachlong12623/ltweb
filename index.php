2a32df863800b9b8784624ba76bb8ab27a320060
Cyonz <95512602+Cyonz@users.noreply.github.com>
2021-12-08 09:46:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,20 +15,50 @@ require 'block/index_func.php';
 </head>
 
 <body>
+    
     <?php
     include "block/header.php";
     ?>
     <div class=containerr>
-        <div class = left>
-           <h3>Tin Thế Giới</h3>
+        <div class=left>
+            <h1>Tin Thế Giới >></h1>
+            <?php
+            $sql = "select * from tin_tuc where Loai_Tin = 'TheGioi' order by MST desc limit 5";
+            $result = mysqli_query($conn, $sql);
+            while ($tin = mysqli_fetch_array($result)) {
+            ?>
+                <div class=fixed>
+                    <img src="<?php echo $tin['Hinh_Anh'] ?>" alt="">
+                    <div>
+                        <a href="#"><b><?php echo $tin['Tieu_De']; ?></b></a>
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
         </div>
         <div class=center>
+            <h1>Tin Tổng Hợp Trong Ngày >></h1>
             <?php
             require 'Body/Tin.php';
             ?>
         </div>
-        <div class = right>
-            sadas
+        <div class=right>
+            <h1>Tin Thể Thao >></h1>
+            <?php
+            $sql = "select * from tin_tuc where Loai_Tin = 'TheThao' order by MST desc limit 5";
+            $result = mysqli_query($conn, $sql);
+            while ($tin = mysqli_fetch_array($result)) {
+            ?>
+                <div class=fixed>
+                    <img src="<?php echo $tin['Hinh_Anh'] ?>" alt="">
+                    <div>
+                        <a href="#"><b><?php echo $tin['Tieu_De']; ?></b></a>
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
         </div>
     </div>
 
