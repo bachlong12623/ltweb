20db70979c148a04c1d5df90f0b2fc2d9a400813
Cyonz <95512602+Cyonz@users.noreply.github.com>
2021-12-08 08:39:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require 'connect/conn.php';
+require 'block/index_func.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,40 +11,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/css_tin.css">
 </head>
 
 <body>
-<?php 
-include "block/header.php";
-?>
-
-<div style="   justify-content: center;
-">
-
-        <div class="news-block">
-            <img src="https://i.pinimg.com/564x/a2/1c/0b/a21c0bdac56742538419f5790836b3ed.jpg" alt="">
+    <?php
+    include "block/header.php";
+    ?>
+    <div class=containerr>
+        <div class = left>
+           <h3>Tin Thế Giới</h3>
         </div>
-
-
-        <div class="news-block">
-            <img src="https://i.pinimg.com/564x/a2/1c/0b/a21c0bdac56742538419f5790836b3ed.jpg" alt="">
-            <div class="demuc">
-                <H3>
-                    Overgeared chapter 1556: Hãy kết hôn với ta
-                    Sehee sẽ không bao giờ quên lần đầu tiên cô ấy nhìn thấy Mercedes.
-                    Vẻ đẹp của cô ấy đập vào mắt cô ngay lập tức.
-                    Mercedes đẹp đến mức khiến cô ngây người ra một lúc.
-                    Mặc dù cô ấy không có thể hiện rõ điều đó trên khuôn mặt của mình.
-                    Sehee tự hỏi liệu cô ấy có phải là một con búp bê không.
-                    Vì vậy, cô ấy càng ngạc nhiên hơn vào lúc này.
-                    'Biểu cảm đó...?'
-                    Gò má của khi Mercedes nhìn về phía sau Grid và Basara hơi ửng đỏ.
-                    Đôi mắt vốn luôn bình tĩnh, bỗng rung động lớn, cảm giác thật tội nghiệp khi thấy chúng đẫm lệ.</H3>
-            </div>
+        <div class=center>
+            <?php
+            require 'Body/Tin.php';
+            ?>
         </div>
-
-
+        <div class = right>
+            sadas
+        </div>
     </div>
+
 </body>
 
 </html>
