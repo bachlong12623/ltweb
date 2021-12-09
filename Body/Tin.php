fc3bbae8644b3beacc8c6b2d10b26995f2f99eb5
Cyonz <95512602+Cyonz@users.noreply.github.com>
2021-12-09 10:22:05 +0700

    Update Tin.php

--- Body/Tin.php
+++ Body/Tin.php
@@ -6,7 +6,11 @@
      <div class=fix>
        <img src="<?php echo $tin['Hinh_Anh'] ?>" alt="">
        <div>
-         <a href="#"><b><?php echo $tin['Tieu_De']; ?></b></a>
+         <a href="Body/Chi_Tiet_Tin.php?id=<?php echo $tin['MST'] ?>"><b><?php echo $tin['Tieu_De']; ?></b></a>
+         <div class="old">
+         <?php echo $rew= substr($tin['Noi_Dung'],0, 298) ?>
+         <a href="Body/Chi_Tiet_Tin.php?id=<?php echo $tin['MST'] ?>">click để xem thêm...</a>
+       </div>
        </div>
      </div>
    <?php
