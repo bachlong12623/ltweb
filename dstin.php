55b501b26646ddfe4e8ead2ff5a2732ca32dc80a
Cyonz <95512602+Cyonz@users.noreply.github.com>
2021-12-10 10:30:55 +0700

    s

--- dstin.php
+++ dstin.php
@@ -17,21 +17,24 @@ $result = mysqli_query($conn, $sql);
 </head>
 
 <body>
-
     <?php
     include "block/header.php";
-    while ($tin = mysqli_fetch_array($result)) {
+    ?>
+    <div class="Big">
+        <?php
+        while ($tin = mysqli_fetch_array($result)) {
 
-    ?>
-        <div class=fixed>
-            <img src="<?php echo $tin['Hinh_Anh'] ?>" alt="">
-            <div>
-                <a href="Chi_Tiet_Tin.php?id=<?php echo $tin['MST'] ?>"><b><?php echo $tin['Tieu_De']; ?></b></a>
+        ?>
+            <div class=fixed>
+                <img src="<?php echo $tin['Hinh_Anh'] ?>" alt="">
+                <div>
+                    <a href="Chi_Tiet_Tin.php?id=<?php echo $tin['MST'] ?>"><b><?php echo $tin['Tieu_De']; ?></b></a>
+                </div>
             </div>
-        </div>
-    <?php
-    }
-    ?>
+        <?php
+        }
+        ?>
+    </div>
 </body>
 
 </html>
